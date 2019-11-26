3875530adfb1d1aa9ac7d02a33e7cb96ab59e473
Johanna Nilsson <nijodotcom@gmail.com>
2019-11-26 09:53:43 +0100

    GET all dogs

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+// GET
+function getAllDogs() {
+  let req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://dog.ceo/api/breeds/list/all");
+  req.send();
+}
+
+function reqListener() {
+  if (this.status === 200) {
+    console.log(this.responseText);
+    let allDogs = JSON.parse(this.responseText).message;
+    console.log(allDogs);
+  } else {
+    console.log("Can´t find dogs");
+  }
+}
+
+getAllDogs();
